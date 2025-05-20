0e351be83364e784a81426d857d2f79072a20e4f
papa matar fall <matar0236@gmail.com>
2025-05-20 14:43:29 +0000

    feature: - Email et message doivent être obligatoirement remplis, message doit être inférieur à 300 caractères.

--- product-trial-master/front/src/app/contacts/ui/contact-form/contact-form.component.ts
+++ product-trial-master/front/src/app/contacts/ui/contact-form/contact-form.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, ButtonModule, CommonModule,
+    InputTextModule,
+    InputNumberModule,
+    InputTextareaModule,],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.css'
+  styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
+  editForm: FormGroup;
+  constructor(private _fb: FormBuilder) {
+    this.editForm = this._fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      message: [null, [Validators.required, Validators.maxLength(300)]]
+    });
+  }
+
+  onFormSubmit() {
+
+  }
 
 }
