94beb2ec2c622a90daa33024c827121516ffee52
papa matar fall <matar0236@gmail.com>
2026-02-04 12:07:33 +0000

    nouvelle mise à jour

--- product-trial-master/kata_backend/src/main/java/com/test/kata_backend/service/UserService.java
+++ product-trial-master/kata_backend/src/main/java/com/test/kata_backend/service/UserService.java
@@ -1,8 +1,9 @@
-package  com.test.kata_backend.service;
+package com.test.kata_backend.service;
+
 import com.test.kata_backend.config.Common;
 import com.test.kata_backend.dto.UserRequest;
-import  com.test.kata_backend.entity.UsersEntity;
-import  com.test.kata_backend.repository.UsersRepository;
+import com.test.kata_backend.entity.UsersEntity;
+import com.test.kata_backend.repository.UsersRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
-
 
 @Service
 @AllArgsConstructor
@@ -43,7 +43,5 @@ public class UserService {
 
         return ResponseEntity.ok(Map.of(Common.message, Common.userNameCreated));
     }
-
-
 
 }
